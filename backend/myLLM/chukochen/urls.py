46ae673b322b28cefd40bf4c5b28ca70e1799602
LeChatelier-Lenz <lechate0222@gmail.com>
2024-07-10 21:05:14 +0800

    modify the backend feature and interfaces

--- backend/myLLM/chukochen/urls.py
+++ backend/myLLM/chukochen/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    # path("voice/", views.read),
-    # path("text/", views.write_answer),
-    path("video/", views.play),
+    path("answer/", views.answer),
 ]
